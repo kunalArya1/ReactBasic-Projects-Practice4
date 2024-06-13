5a28cd923848a66a2c720e34e19bd4a4f64a0171
kunalArya1 <kunalkrraj220@gmail.com>
2024-06-13 14:05:41 +0530

    Project 7 Completed

---
+++ Project_7/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import LifeGame from "./Components/LifeGame";
+
+const App = () => {
+  return (
+    <div>
+      <LifeGame />
+    </div>
+  );
+};
+
+export default App;
